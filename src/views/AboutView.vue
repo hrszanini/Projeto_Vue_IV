33294885f1c1da277fcc6acaf9cde87df726588b
Hamilton Zanini <hrszanini@gmail.com>
2022-09-07 00:00:43 +0000

    Atualização

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Tá sem ter o que fazer, meu parça?</h1>
   </div>
 </template>
---
+++ src/views/UsuarioView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="usuario">
+
+        <p>
+            <input type="text" v-model="name">
+            <input type="button" value="OK" @click="inserirUsuario()">
+        </p>
+
+        <ui v-if="usuarios.length > 0">
+            <li v-for="usuario in usuarios" :key="usuario">
+                {{usuario}}
+            </li>
+        </ui>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name : "Hamilton",
+            usuarios : []
+        }
+    },
+    methods: {
+        inserirUsuario() {
+            if(this.name != ""){
+                this.usuarios.push(this.name);
+                this.name = "";
+            }
+        }
+    }
+}
+</script>
